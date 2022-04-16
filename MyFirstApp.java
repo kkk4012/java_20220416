11bb8ff4d99f45fd5d51b20d4d60932d1f072062
kkk4012 <103571506+kkk4012@users.noreply.github.com>
2022-04-16 13:40:09 +0800

    Add files via upload

---
+++ MyFirstApp.java
@@ -0,0 +1,75 @@
+public class MyFirstApp {
+	// 주석 - 메모하거나, 안쓰는 코드를 잠깐 가릴 때
+	{
+		{
+			{
+				
+			}
+		}
+	}
+	//	코드1
+	//	ㅁㅇㄻㅇㄴㄹ
+	//{} : 영역 표현
+	//새로운 영역이 열리면 들여쓰기 (indent)
+	//단축키 : 탭(tab), 쉬프트+탭
+	//시작지점(main) 작성 -> 메인메서드
+	public static void main(String[] args) {
+		//콘솔에 출력
+		//; (세미콜론) ->하나의 명령을 마무리함.(명령 구분을 위함)
+		//프로그램 시작 단축키 f11
+//		System.out.println(100);
+//		System.out.println("hihihihi");
+//		System.out.println("김정현"); //문자 표현과 명령어 표현 구분 ->""유무
+		//프로그램은 위에서 아래로 순차 실행
+		
+		//값 ==> 데이터 ==> 자료
+		//자료형(데이터 타입)
+//		System.out.println(200); //정수(int)
+//		System.out.println("200");//문자열(String)
+//		System.out.println(200.0);//실수(double)
+//		//자료형 -> 프로그래밍의 50% 이상의 지분
+//		System.out.println(300);
+//		System.out.println(300.0);
+//		System.out.println("300");
+		
+		//값을 저장 한다. -> 변수에 저장
+		//상자만들기 -> 변수 선언
+		//상자에 어떤 유형의 데이터를 저장할것이며. 상자를 어떻게 찾을 것이냐
+//		int num;
+		
+		//상자에 저장 -> 변수에 대입(=사용)
+		//int num = 300; //중복선언
+		//300 = num // 저장소 왼쪽, 값이 오른쪽
+		
+		//변수가 = 왼쪽에 올때만 저장 역할. 그외에는 저장하고있는 값으로 사용
+		//int num1;
+		//int num2;
+		//num1= 10;
+		//num2= 20; num1,2 이름이다르므로 다른저장소로 된다.
+		//num1 =num2; //num1 = 20
+		//num2 = num2 // num2 와 num2가 같다. X -> num2 =20
+	
+		
+//		num = 300;
+		
+		
+		//200을 정수로 30번 출력?
+//		System.out.println(num);
+//		System.out.println(num);
+//		System.out.println(num);
+//		
+		//명명규칙
+		//숫자로 시작 X
+		//특수문자 X
+		//공백 x
+		//소문자로 시작.
+		//의미가 바뀔 때마다 대문자로 구분
+		//변수명을 a,b 의미 없는 이름으로 짓지 않는다.
+		int num3 = 30;
+//		int num3;
+//		num3 = 30; 윗줄처럼 한줄로 쓸수있다.
+		String str1= "hello";
+		double dnum = 3.14;
+	}
+}
+	
